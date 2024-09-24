78d2d7f2dde2baff87dbbc1e3c1be251d6beea72
KiwiPheonix <peckerisawesome@outlook.com>
2024-09-24 16:39:13 -0700

    abstraction of autonomous testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/opmode/autonomous/left/LeftSideAuto.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.robot.opmode.autonomous.left;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+interface LeftSideAuto {
+
+    default void initialize(HardwareMap hardwareMap) {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/opmode/autonomous/right/RightSideAuto.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.robot.opmode.autonomous.right;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+interface RightSideAuto {
+    default void initialize(HardwareMap hardwareMap) {
+
+    }
+}
