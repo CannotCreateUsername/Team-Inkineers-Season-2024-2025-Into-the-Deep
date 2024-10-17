1f88c38d4fb3c52334efcd2da7052121c5025d1f
KiwiPheonix <peckerisawesome@outlook.com>
2024-10-16 17:49:38 -0700

    Beeline Auto Prep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -224,8 +224,14 @@ public final class TankDrive {
         // TODO: make sure your config has motors with these names (or change them)
         //   add additional motors on each side if you have them
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
-        leftMotors = Arrays.asList(hardwareMap.get(DcMotorEx.class, "left"));
-        rightMotors = Arrays.asList(hardwareMap.get(DcMotorEx.class, "right"));
+        leftMotors = Arrays.asList(
+                hardwareMap.get(DcMotorEx.class, "left"),
+                hardwareMap.get(DcMotorEx.class, "left2")
+        );
+        rightMotors = Arrays.asList(
+                hardwareMap.get(DcMotorEx.class, "right"),
+                hardwareMap.get(DcMotorEx.class, "right2")
+                );
 
         for (DcMotorEx m : leftMotors) {
             m.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -235,7 +241,7 @@ public final class TankDrive {
         }
 
         // TODO: reverse motor directions if needed
-        //   leftMotors.get(0).setDirection(DcMotorSimple.Direction.REVERSE);
+        leftMotors.get(0).setDirection(DcMotorSimple.Direction.REVERSE);
 
         // TODO: make sure your config has an IMU with this name (can be BNO or BHI)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/opmode/autonomous/right/RightSideAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/opmode/autonomous/right/RightSideAuto.java
@@ -1,14 +1,20 @@
 package org.firstinspires.ftc.teamcode.robot.opmode.autonomous.right;
 
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.robot.subsystem.ArmSubsystem;
+import org.firstinspires.ftc.teamcode.TankDrive;
 import org.firstinspires.ftc.teamcode.robot.subsystem.IntakeSubsystem;
 
 abstract class RightSideAuto extends LinearOpMode {
 
     IntakeSubsystem intakeSubsystem = new IntakeSubsystem(hardwareMap);
-    ArmSubsystem armSubsystem = new ArmSubsystem(hardwareMap);
+    TankDrive drive = new TankDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+    Action runToChamber = drive.actionBuilder(new Pose2d(0, 0, 0))
+            .lineToY(20)
+            .build();
 
     void run() {
         // Put Code Here for autonomous
@@ -16,6 +22,8 @@ abstract class RightSideAuto extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        // Add initialization code here
+        waitForStart();
         run();
     }
 }
